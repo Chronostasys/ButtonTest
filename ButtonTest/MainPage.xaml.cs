26d7586140ac2c21ad641677c4a4aaab23d62e13
Chronos <1769712655@qq.com>
2021-08-14 19:22:17 +0800

    Add project files.

---
+++ ButtonTest/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace ButtonTest
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		int count = 0;
+		private async void OnCounterClicked(object sender, EventArgs e)
+		{
+			await FilePicker.PickAsync();
+			count++;
+			CounterLabel.Text = $"Current count: {count}";
+		}
+	}
+}
---
+++ ButtonTest/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace ButtonTest
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
